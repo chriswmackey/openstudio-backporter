83133f5e806fab9e2ca44810c4392899f0f23df7
Chris Mackey <chris@ladybug.tools>
2026-06-09 11:58:59 -0700

    Respond to all review comments

--- openstudiobackporter/backport_3_8_0_to_3_7_0.py
+++ openstudiobackporter/backport_3_8_0_to_3_7_0.py
@@ -5,7 +5,7 @@ from openstudiobackporter.helpers import (
     brief_description,
     copy_object_as_is,
     copy_with_added_fields,
-    copy_with_deleted_fields,
+    copy_with_deleted_fields
 )
 
 
@@ -47,12 +47,42 @@ def run_translation(idf_3_8_0: openstudio.IdfFile) -> openstudio.IdfFile:
             # * Latent Effectiveness of Heating Air Flow Curve Name * 21
             # * Sensible Effectiveness of Cooling Air Flow Curve Name * 22
             # * Latent Effectiveness of Cooling Air Flow Curve Name * 23
-            copy_from_indices = (4, 5, 6, 7)
-            copy_to_indices = (6, 7, 10, 11)
-            copy_with_added_fields(obj=obj, newObject=newObject, skip_indices=set(copy_to_indices))
-            for cf, ct in zip(copy_from_indices, copy_to_indices):
-                if value := obj.getString(cf):
-                    newObject.setString(ct, value.get())
+
+            # copy the object while inserting fields for the Effectiveness at 75%
+            eff_75_indices = (6, 7, 10, 11)
+            eff_100_indices = (4, 5, 6, 7)
+            eff_curve_indices = (20, 21, 22, 23)
+            copy_with_added_fields(obj=obj, newObject=newObject, inserted_indices=set(eff_75_indices))
+
+            # loop through the effectiveness curves and convert them
+            for e100, e75, ec in zip(eff_100_indices, eff_75_indices, eff_curve_indices):
+                curve_id = openstudio.toUUID(obj.getField(ec).get())
+                curve_obj = idf_3_8_0.getObject(curve_id)
+                if curve_obj:
+                    curve_obj = curve_obj.get()
+                    curve_idd_name = curve_obj.iddObject().name()
+                    if curve_idd_name == "OS:Table:Lookup":  # pull the value from the table
+                        if e75_value := curve_obj.getDouble(11):
+                            newObject.setDouble(e75, e75_value.get())
+                    elif curve_idd_name == "OS:Curve:Quadratic":  # reverse translate the curve and evaluate it
+                        # collect all of the objects the curve references
+                        temp_model = openstudio.model.Model()
+                        hx_curve = openstudio.model.CurveQuadratic(temp_model)
+                        if coeff_value := curve_obj.getDouble(2):
+                            hx_curve.setCoefficient1Constant(coeff_value.get())
+                        if coeff_value := curve_obj.getDouble(3):
+                            hx_curve.setCoefficient2x(coeff_value.get())
+                        if coeff_value := curve_obj.getDouble(4):
+                            hx_curve.setCoefficient3xPOW2(coeff_value.get())
+                        if e100_value := obj.getDouble(e100):
+                            e100_value = e100_value.get()
+                            print(hx_curve.evaluate(0.75) * e100_value)
+                            newObject.setDouble(e75, hx_curve.evaluate(0.75) * e100_value)
+
+                else:  # if no curve has been assigned, assume a constant effectiveness
+                    if value := obj.getString(e100):
+                        newObject.setString(e75, value.get())
+
             targetIdf.addObject(newObject)
 
         elif (
@@ -87,7 +117,7 @@ def run_translation(idf_3_8_0: openstudio.IdfFile) -> openstudio.IdfFile:
             # ----------------------------------------------
             # * Mean Radiant Temperature Calculation Type * 10
             #   * ZoneAveraged -> EnclosureAveraged
-            copy_with_deleted_fields(obj=obj, newObject=newObject, skip_indices={10})
+            copy_object_as_is(obj=obj, newObject=newObject)
             if value := obj.getString(10):
                 value = value.get()
                 value = 'ZoneAveraged' if value == 'EnclosureAveraged' else value
